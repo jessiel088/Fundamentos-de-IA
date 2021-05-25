ae9bd51e72e0830ce641b117564e44c64a5c3366
jessiel088 <79344413+jessiel088@users.noreply.github.com>
2021-05-24 18:06:06 -0600

    Add files via upload

---
+++ Reconocimiento Facial/entrenamientoRF.py
@@ -0,0 +1,33 @@
+import cv2
+import os
+import numpy as np
+
+dataPath = 'C:/Users/jessi/Downloads/Reconocimiento Facial/Data'
+peopleList = os.listdir(dataPath)
+print('Lista de personas: ', peopleList)
+
+labels = []
+facesData = []
+label = 0
+
+for nameDir in peopleList:
+	personPath = dataPath + '/' + nameDir
+	print('Leyendo las imágenes')
+
+	for fileName in os.listdir(personPath):
+		print('Rostros: ', nameDir + '/' + fileName)
+		labels.append(label)
+		facesData.append(cv2.imread(personPath+'/'+fileName,0))
+		image = cv2.imread(personPath+'/'+fileName,0)
+	label = label + 1
+
+# Métodos para entrenar el reconocedor
+face_recognizer = cv2.face.EigenFaceRecognizer_create()
+
+# Entrenando el reconocedor de rostros
+print("Entrenando...")
+face_recognizer.train(facesData, np.array(labels))
+
+# Almacenando el modelo obtenido
+face_recognizer.write('modeloEigenFace.xml')
+print("Modelo almacenado...")
